5ddb52c1ceb333269d725fa71ca902da6d4aa8d0
Jessie811849 <110365423+Jessie811849@users.noreply.github.com>
2025-11-25 14:14:44 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
+void hanoi(int n, char from, char to, char aux) {
+    if (n == 1) {
+        cout << "Move ring " << 1 << " from " << from << " aux " << aux << endl;
+        return;
+    }
+    hanoi(n - 1, from, aux, to);              
+    cout << "Move ring " << n << " from " << from << " aux " << aux << endl; 
+    hanoi(n - 1, to, from, aux);              
 
 int main() {
-    
+    int N;
+    while (cin >> N) {
+        hanoi(N, 'A', 'B', 'C');
+    }
 
     return 0;
 }
